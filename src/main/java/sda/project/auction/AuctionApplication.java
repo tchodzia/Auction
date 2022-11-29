25570dcbe2e61f808b9a66d31a69dc99c38d27b5
tchodzia <tchodzia@gmail.com>
2022-11-29 20:12:04 +0100

    Initial commit

---
+++ src/main/java/sda/project/auction/AuctionApplication.java
@@ -0,0 +1,13 @@
+package sda.project.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionApplication.class, args);
+	}
+
+}
