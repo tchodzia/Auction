e0e56128867b120e4c2efbf30b903987ceafbf9c
tchodzia <tchodzia@gmail.com>
2022-11-30 21:19:39 +0100

    feature: AuctionService, AuctionRepository fixed: index.html, WelcomeController

--- src/main/java/sda/project/auction/web/mvc/WelcomeController.java
+++ src/main/java/sda/project/auction/web/mvc/WelcomeController.java
@@ -1,6 +1,8 @@
 package sda.project.auction.web.mvc;
 
+import sda.project.auction.model.Auction;
 import sda.project.auction.model.User;
+import sda.project.auction.service.AuctionService;
 import sda.project.auction.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,16 +10,22 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("")
 public class WelcomeController {
     private final UserService userService;
+    private final AuctionService auctionService;
     @GetMapping("/")
     public String welcomePage(ModelMap map) {
-        User user = userService.findById(1L);
+        //User user = userService.findById(1L);
         //map.addAttribute("userName", "Jan Kowalski");
-        map.addAttribute("user", user);
+        //map.addAttribute("user", user);
+
+        List<Auction> auctions = auctionService.findAll();
+        map.addAttribute("auctions", auctions);
         return "index";
     }
 }
