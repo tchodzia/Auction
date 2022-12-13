500ea2debf046bddf4fce3f1fb0e954b21a8954c
Balarama <herrmaan91@gmail.com>
2022-12-13 18:31:15 +0100

    fixed : updating user after implmentation of auth

--- src/main/java/sda/project/auction/web/mappers/UserMapper.java
+++ src/main/java/sda/project/auction/web/mappers/UserMapper.java
@@ -6,7 +6,7 @@ import sda.project.auction.web.form.CreateUserForm;
 public class UserMapper {
 
     public static User toEntity(CreateUserForm form){
-        return new User(form.getEmail(), form.getPassword(), form.getAccount_name(), form.getVoivodeship(), form.getCity(), form.getAddress(), form.getRole());
+        return new User(form.getEmail(), form.getPassword(), form.getAccount_name(), form.getVoivodeship(), form.getCity(), form.getAddress(), form.getUserRole());
     }
 
     public static User toUpdateEntity(User user, CreateUserForm form){
