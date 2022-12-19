8dc7771a2c71e23c6ff540ce170c66397b36c860
Balarama <herrmaan91@gmail.com>
2022-12-19 19:46:55 +0100

    update : showing problem of wrong input to the form

--- src/main/java/sda/project/auction/web/form/BidForm.java
+++ src/main/java/sda/project/auction/web/form/BidForm.java
@@ -1,7 +1,9 @@
 package sda.project.auction.web.form;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Min;
 import lombok.*;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 @Setter
@@ -10,14 +12,21 @@ import lombok.*;
 @NoArgsConstructor
 public class BidForm {
 
-    public BidForm(Long auction_id, Long user_id) {
+
+    public BidForm(Long auction_id, Long user_id, Long currentPrice) {
         this.auction_id = auction_id;
         this.user_id = user_id;
+        this.currentPrice = currentPrice;
     }
-
     private Long auction_id;
     private Long user_id;
 
+    private Long currentPrice;
+
     private Long amount;
 
+    @AssertTrue
+    public boolean isOk(){
+        return amount > currentPrice;
+    }
 }
